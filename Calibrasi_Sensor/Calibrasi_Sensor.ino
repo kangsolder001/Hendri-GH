803cbb5f0abf869e5d38154ea613fe85880d6f0e
mukhlisadigunawan <mukhlisadigunawan@gmail.com>
2021-06-21 22:04:32 +0700

    Update Calibrasi_Sensor.ino

--- Calibrasi_Sensor/Calibrasi_Sensor.ino
+++ Calibrasi_Sensor/Calibrasi_Sensor.ino
@@ -2,7 +2,7 @@
 #define PB_A 5
 #define PB_B 6
 #define buzz 2
-unsigned int prevCalEC,prevPBA,Start_PBA;
+unsigned int prevCalEC, prevPBA, Start_PBA,Start_PBB;
 bool b1st;
 void setup() {
   Serial.begin(115200);
@@ -18,42 +18,90 @@ void loop() {
   PBB = rPB(PB_B);
   //Serial.print("PBA = ");
   //Serial.println(PBA);
-  
-  if (!PBA && Mode != "calEC A")
+  if ( PBA &&  Mode != "doneA")
   {
-    if ((millis() - prevPBA > 100) && b1st)
-    {
-      if (b1st)
-      {
-        digitalWrite(buzz, 1);
-        Serial.println("LED ON ");
-        b1st = false;
-      }
-      prevPBA = millis();
-    }
-    else if (( millis() - prevPBA > 100) && !b1st)
-    {
-      if (!b1st)
-      {
-        digitalWrite(buzz, 0);
-        Serial.println("LED OFF");
-        b1st = true;
-      }
-    }
+    Start_PBA = millis();
+  }
+  else if ( PBA && Mode == "doneA")
+  {
+    Start_PBB = millis();
+  }
+
+  else if (!PBA  && Mode != "doneA")
+  {
     if ( millis() - Start_PBA > 3000)
     {
       Mode = "calEC A";
       Serial.println("calEC A");
+
+      Start_PBA = millis();
+      for ( int i = 0 ; i < 2 ; i++)
+      {
+        digitalWrite(buzz, 1);
+        delay(100);
+        digitalWrite(buzz, 0);
+        delay(100);
+      }
       digitalWrite(buzz, 1);
       Serial.println("LED ON ");
-      Start_PBA = millis();
       b1st = true;
-
     }
   }
-  else if ( PBA && Mode != "calEC A")
+  else if (!PBA  && Mode == "doneA")
   {
-    Start_PBA = millis();
+    if ( millis() - Start_PBB > 3000)
+    {
+      Mode = "calEC B";
+      Serial.println("calEC B");
+
+      Start_PBB = millis();
+      for ( int i = 0 ; i < 2 ; i++)
+      {
+        digitalWrite(buzz, 1);
+        delay(100);
+        digitalWrite(buzz, 0);
+        delay(100);
+      }
+      digitalWrite(buzz, 1);
+      Serial.println("LED ON ");
+      b1st = true;
+    }
+  }
+
+
+  if ( Mode == "calEC A")
+  {
+    if (b1st && millis() - Start_PBA > 1000)
+    {
+      digitalWrite(buzz, 0);
+      Serial.println("LED OFF");
+      b1st = false;
+    }
+    calEC(btsA);
+    //    Serial.println(btsA);
+    if ( btsA != 0)
+    {
+      Serial.println("DONE A ");
+      Serial.print("bts A = ");
+      Serial.println(btsA);
+      Mode = "done A";
+    }
+  }
+  else if ( Mode == "calEC B")
+  {
+    if (b1st && millis() - Start_PBB > 1000)
+    {
+      digitalWrite(buzz, 0);
+      Serial.println("LED OFF");
+      b1st = false;
+    }
+    calEC(btsB);
+    if ( btsB != 0)
+    {
+      Serial.println("DONE B ");
+      Serial.print("bts B = ");
+      Serial.println(btsB);
+    }
   }
 
 
@@ -88,30 +136,4 @@ void loop() {
     }
   }
 
-  if ( Mode == "calEC A")
-  {
-    if (b1st && millis() - Start_PBA > 2000)
-    {
-      digitalWrite(buzz, 0);
-      Serial.println("LED OFF");
-    }
-    calEC(btsA);
-    //    Serial.println(btsA);
-    if ( btsA != 0)
-    {
-      Serial.println("DONE A ");
-      Serial.print("bts A = ");
-      Serial.println(btsA);
-    }
-  }
-  else if ( Mode == "calEC B")
-  {
-    calEC(btsB);
-    if ( btsB != 0)
-    {
-      Serial.println("DONE B ");
-      Serial.print("bts B = ");
-      Serial.println(btsB);
-    }
-  }
 }
